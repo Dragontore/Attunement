5e918bc85f0c7af2972b11ca2c6e3d2c4b487de3
Dragontore <dragontore@hotmail.co.uk>
2020-02-20 16:49:26 +0000

    meshes added to base room

--- Source/Attunement/Private/Rooms/BaseRoom.cpp
+++ Source/Attunement/Private/Rooms/BaseRoom.cpp
@@ -3,11 +3,34 @@
 
 #include "BaseRoom.h"
 
+#include "Net/UnrealNetwork.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/PlayerStart.h"
+
 // Sets default values
 ABaseRoom::ABaseRoom()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	PlayerStart = CreateDefaultSubobject<APlayerStart>("Player Start");
+
+	FloorMesh = CreateDefaultSubobject<UStaticMeshComponent>("Floor Mesh");
+	RootComponent = FloorMesh;
+
+	RoofMesh = CreateDefaultSubobject<UStaticMeshComponent>("Roof Mesh");
+	RoofMesh->SetupAttachment(RootComponent);
+
+	Wall1Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Wall 1 Mesh");
+	Wall1Mesh->SetupAttachment(RootComponent);
+
+	Wall2Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Wall 2 Mesh");
+	Wall2Mesh->SetupAttachment(RootComponent);
+
+	Wall3Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Wall 3 Mesh");
+	Wall3Mesh->SetupAttachment(RootComponent);
+
+	Wall4Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Wall 4 Mesh");
+	Wall4Mesh->SetupAttachment(RootComponent);
 
 }
 
@@ -18,10 +41,21 @@ void ABaseRoom::BeginPlay()
 	
 }
 
-// Called every frame
-void ABaseRoom::Tick(float DeltaTime)
+void  ABaseRoom::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
-	Super::Tick(DeltaTime);
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Replicate to every client, no special condition required
+	// 
+
+	DOREPLIFETIME(ABaseRoom, FloorMesh);
+	DOREPLIFETIME(ABaseRoom, RoofMesh);
+	DOREPLIFETIME(ABaseRoom, Wall1Mesh);
+	DOREPLIFETIME(ABaseRoom, Wall2Mesh);
+	DOREPLIFETIME(ABaseRoom, Wall3Mesh);
+	DOREPLIFETIME(ABaseRoom, Wall4Mesh);
 
 }
+
+
 
--- Source/Attunement/Public/Rooms/BaseRoom.h
+++ Source/Attunement/Public/Rooms/BaseRoom.h
@@ -6,21 +6,56 @@
 #include "GameFramework/Actor.h"
 #include "BaseRoom.generated.h"
 
+class UStaticMeshComponent;
+class APlayerStart;
+
 UCLASS()
 class ATTUNEMENT_API ABaseRoom : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
+	// Non Blueprint Public Functions
 	// Sets default values for this actor's properties
 	ABaseRoom();
 
+public:
+	// Blueprint Public Functions
+public:
+	//  Non Blueprint Public Varibles
+public:
+	// Blueprint Public Varibles
 protected:
+	// Non Blueprint Protected Functions
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+protected:
+	// Blueprint Protected Functions
+protected:
+	// Non Blueprint Protected Varibles
+protected:
+	// Blueprint Protected Varibles
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Meshes")
+	UStaticMeshComponent* RoofMesh;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Meshes")
+	UStaticMeshComponent* FloorMesh;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Meshes")
+	UStaticMeshComponent* Wall1Mesh;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Meshes")
+	UStaticMeshComponent* Wall2Mesh;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Meshes")
+	UStaticMeshComponent* Wall3Mesh;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Meshes")
+	UStaticMeshComponent* Wall4Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Meshes")
+	APlayerStart* PlayerStart;
+
 
 };
