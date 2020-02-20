71daed6369a8d9f727cbd8a5810c705df6b5b224
Dragontore <dragontore@hotmail.co.uk>
2020-02-20 20:38:47 +0000

    Judgement room added

---
+++ Source/Attunement/Public/Rooms/JudgementStartRoom.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Rooms/BaseRoom.h"
+#include "JudgementStartRoom.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATTUNEMENT_API AJudgementStartRoom : public ABaseRoom
+{
+	GENERATED_BODY()
+	
+};
